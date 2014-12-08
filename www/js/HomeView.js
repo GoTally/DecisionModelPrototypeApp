b85d1520c0aeea8211714ccf24e214e6a05875c4
Tyler Ewing <tewing10@gmail.com>
2014-12-07 19:02:38 -0500

    Add in API call to populate home view

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -12,7 +12,26 @@ var HomeView = function() {
 
   this.render = function() {
     this.el.html(HomeView.template());
+    app.user_id=4;
+    var url = 'https://decision-prototype.herokuapp.com/users/'+app.user_id;
+
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      data: {polls: true},
+      success: function(response) {
+        $.each(response.polls, function(index, object) {
+          //$('.poll-list').append('<li class="list__item list__item--tappable">'+object.title+'</li>')
+          $('.poll-list').append(HomeView.polltemplate(object));
+        });
+      }
+    });
     // Make API call to server and get polls with user ID
+    /*$.get(url, { polls: true }, function(response) {
+      app.showAlert(response.polls);
+      $('.poll-list').html(HomeView.litemplate(response.polls));
+    });*/
+
     return this;
   };
 
@@ -26,3 +45,4 @@ var HomeView = function() {
 }
 
 HomeView.template = Handlebars.compile($('#home-tpl').html());
+HomeView.polltemplate = Handlebars.compile($('#home-poll-tpl').html());
