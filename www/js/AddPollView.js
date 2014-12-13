e0be914a92468c4d7c152ecca1f5a69dc1289a0c
Tyler Ewing <tewing10@gmail.com>
2014-12-13 13:08:46 -0500

    Stub list item links to views

--- www/js/AddPollView.js
+++ www/js/AddPollView.js
@@ -8,6 +8,15 @@ var AddPollView = function() {
     app.showAlert('Send data to server');
   };
 
+  this.deadline = function() {
+  };
+
+  this.choices = function() {
+  };
+
+  this.people = function() {
+  };
+
   this.render = function() {
     this.el.html(AddPollView.template());
     return this;
@@ -17,6 +26,9 @@ var AddPollView = function() {
     this.el = $('<div/>');
     this.el.on('click', '#add-poll-back-btn', this.back)
     this.el.on('click', '#add-poll-send-btn', this.send)
+    this.el.on('click', '#add-poll-deadline', this.deadline)
+    this.el.on('click', '#add-poll-choices', this.choices)
+    this.el.on('click', '#add-poll-people', this.people)
   };
 
   this.initialize(); 
--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -11,6 +11,7 @@ var HomeView = function() {
 
   this.render = function() {
     this.el.html(HomeView.template());
+    //app.user_id = 4; // For local dev
     var url = 'https://decision-prototype.herokuapp.com/users/'+app.user_id;
 
     $.ajax({
