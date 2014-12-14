98955f0224b0c4212b09cc8757b5daf8c4b6d964
Tyler Ewing <tewing10@gmail.com>
2014-12-14 13:11:16 -0500

    Add in times for deadline and persist value

--- www/js/DeadlineView.js
+++ www/js/DeadlineView.js
@@ -1,15 +1,34 @@
 var DeadlineView = function() {
 
+  this.times = ['None', '10 minutes', '30 minutes', '1 hour', '2 hours'];
+
+  this.ok = function() {
+    var selectedTime = $("input[type='radio']:checked").data('time');
+    AddPollView.deadline = selectedTime;
+    window.location.hash = '#newpoll';
+  };
+
   this.render = function() {
     this.el.html(DeadlineView.template());
+    var list = this.el.find('#deadline-list');
+    $.each(this.times, function(index, object) {
+      list.append(DeadlineView.timetemplate(object));
+    });
+    // This doesn't work as expected
+    if (AddPollView.deadline) {
+      $("input[data-time='"+AddPollView.deadline+"']")[0].checked = true;
+    }
+
     return this;
   };
 
   this.initialize = function() {
     this.el = $('<div/>');
+    this.el.on('click', '#deadline-ok-btn', this.ok);
   };
 
   this.initialize();
 }
 
 DeadlineView.template = Handlebars.compile($('#deadline-tpl').html());
+DeadlineView.timetemplate = Handlebars.compile($('#deadline-time-tpl').html());
