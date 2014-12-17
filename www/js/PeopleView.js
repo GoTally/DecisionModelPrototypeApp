06bc3fbd99fad654dd48c6ce4b1bf21492f096db
Tyler Ewing <tewing10@gmail.com>
2014-12-17 00:46:50 -0500

    Fill in logic i dunno 🍺  :beer:

--- www/js/PeopleView.js
+++ www/js/PeopleView.js
@@ -1,12 +1,41 @@
 var PeopleView = function() {
 
+  this.ok = function() {
+    AddPollView.people = [];
+    var people = $("#people-list input[type='checkbox']:checked")
+    $.each(people, function(index, object) {
+      AddPollView.people.push($(object).data('person'));
+    });
+    window.location.hash= '#newpoll';
+  };
+
   this.render = function() {
     this.el.html(PeopleView.template());
+
+    $.ajax({
+      url: 'https://decision-prototype.herokuapp.com/users',
+      dataType: 'jsonp',
+      success: function(response) {
+        $.each(response, function(index, object) {
+          var full_name = object.first_name + ' ' + object.last_name;
+          $('#people-list').append(PeopleView.persontemplate(full_name));
+        });
+      }
+    })
+
+    // This doesn't work as expected
+    if (AddPollView.people) {
+      $.each(AddPollView.people, function(index, object) {
+        $("#people-list input[data-person='"+object+"']")[0].checked = true;
+      });
+    }
+
     return this;
   };
 
   this.initialize = function() {
     this.el = $('<div/>');
+    this.el.on('click', '#people-ok-btn', this.ok);
   };
 
   this.initialize();
